ac1539bf4c2668a47a70732c839fbbbcf791c736
Omar <tarekes68@gmail.com>
2025-01-24 17:39:50 +0200

    added UIConfig and Utilites Static Classes to aid with development

---
+++ Desktop/Utilites.cs
@@ -0,0 +1,24 @@
+﻿using Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Desktop
+{
+    static class Utilites
+    {
+        public static Person? Person;
+
+        public static void InitForm(Form form , string name)
+        {
+            form.Text = name;
+            form.BackColor = UIConfig.SecondaryColorDark;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.MaximizeBox = false;
+        }
+    }
+}
