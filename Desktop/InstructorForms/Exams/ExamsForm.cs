246047f183e354cda342ff1b7b7b154409383db3
Omar <tarekes68@gmail.com>
2025-01-25 19:18:54 +0200

    created the basis for Questions, Students and Exam Forms for Instructor User

---
+++ Desktop/InstructorForms/Exams/ExamsForm.cs
@@ -0,0 +1,31 @@
+﻿using Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop.InstructorForms.Exams
+{
+    public partial class ExamsForm : Form
+    {
+        Form super;
+        Course course;
+        public ExamsForm(Form super, Course course)
+        {
+            InitializeComponent();
+            this.super = super;
+            this.course = course;
+            Utilites.InitForm(this, $"Exams of '{course.Name}'");
+        }
+
+        private void ExamsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            super.Show();
+        }
+    }
+}
