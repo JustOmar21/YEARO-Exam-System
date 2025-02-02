3e8467915114e609d5d83ab326efcc5c7a63d858
Omar <tarekes68@gmail.com>
2025-02-03 00:07:27 +0200

    Added Course and Topics Report

---
+++ Desktop/Admin/CourseTopicForm/CourseTopicForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Desktop.Admin.CourseTopicForm
+{
+    partial class CourseTopicForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            returnBTN = new Button();
+            topicDATA = new DataGridView();
+            courseLBL = new Label();
+            courseLIST = new ComboBox();
+            showBTN = new Button();
+            nameTXT = new RichTextBox();
+            idTXT = new RichTextBox();
+            nameLBL = new Label();
+            idLBL = new Label();
+            ((System.ComponentModel.ISupportInitialize)topicDATA).BeginInit();
+            SuspendLayout();
+            // 
+            // returnBTN
+            // 
+            returnBTN.Font = new Font("Segoe UI", 25F);
+            returnBTN.ForeColor = SystemColors.ControlText;
+            returnBTN.Location = new Point(12, 12);
+            returnBTN.Name = "returnBTN";
+            returnBTN.Size = new Size(1156, 76);
+            returnBTN.TabIndex = 15;
+            returnBTN.Text = "Return";
+            returnBTN.UseVisualStyleBackColor = true;
+            returnBTN.Click += returnBTN_Click;
+            // 
+            // topicDATA
+            // 
+            topicDATA.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            topicDATA.DefaultCellStyle = dataGridViewCellStyle1;
+            topicDATA.Location = new Point(12, 94);
+            topicDATA.Name = "topicDATA";
+            topicDATA.ReadOnly = true;
+            topicDATA.RowHeadersWidth = 51;
+            topicDATA.Size = new Size(1156, 927);
+            topicDATA.TabIndex = 12;
+            topicDATA.CellDoubleClick += topicDATA_CellDoubleClick;
+            // 
+            // courseLBL
+            // 
+            courseLBL.AutoSize = true;
+            courseLBL.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            courseLBL.Location = new Point(1174, 94);
+            courseLBL.Name = "courseLBL";
+            courseLBL.Size = new Size(160, 57);
+            courseLBL.TabIndex = 16;
+            courseLBL.Text = "Course";
+            // 
+            // courseLIST
+            // 
+            courseLIST.DropDownStyle = ComboBoxStyle.DropDownList;
+            courseLIST.Font = new Font("Segoe UI", 25F);
+            courseLIST.FormattingEnabled = true;
+            courseLIST.Location = new Point(1174, 168);
+            courseLIST.Name = "courseLIST";
+            courseLIST.Size = new Size(716, 65);
+            courseLIST.TabIndex = 17;
+            // 
+            // showBTN
+            // 
+            showBTN.Font = new Font("Segoe UI", 25F);
+            showBTN.Location = new Point(1174, 887);
+            showBTN.Name = "showBTN";
+            showBTN.Size = new Size(716, 134);
+            showBTN.TabIndex = 18;
+            showBTN.Text = "Get Topics";
+            showBTN.UseVisualStyleBackColor = true;
+            showBTN.Click += showBTN_Click;
+            // 
+            // nameTXT
+            // 
+            nameTXT.BackColor = SystemColors.Control;
+            nameTXT.BorderStyle = BorderStyle.None;
+            nameTXT.Font = new Font("Segoe UI", 20F);
+            nameTXT.Location = new Point(1221, 594);
+            nameTXT.Name = "nameTXT";
+            nameTXT.ReadOnly = true;
+            nameTXT.Size = new Size(669, 120);
+            nameTXT.TabIndex = 22;
+            nameTXT.Text = "";
+            nameTXT.MouseDown += idTXT_MouseDown;
+            // 
+            // idTXT
+            // 
+            idTXT.BackColor = SystemColors.Control;
+            idTXT.BorderStyle = BorderStyle.None;
+            idTXT.Font = new Font("Segoe UI", 20F);
+            idTXT.Location = new Point(1221, 394);
+            idTXT.Name = "idTXT";
+            idTXT.ReadOnly = true;
+            idTXT.Size = new Size(669, 120);
+            idTXT.TabIndex = 21;
+            idTXT.Text = "";
+            idTXT.MouseDown += idTXT_MouseDown;
+            // 
+            // nameLBL
+            // 
+            nameLBL.AutoSize = true;
+            nameLBL.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            nameLBL.Location = new Point(1174, 517);
+            nameLBL.Name = "nameLBL";
+            nameLBL.Size = new Size(114, 46);
+            nameLBL.TabIndex = 20;
+            nameLBL.Text = "Name";
+            // 
+            // idLBL
+            // 
+            idLBL.AutoSize = true;
+            idLBL.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            idLBL.Location = new Point(1174, 345);
+            idLBL.Name = "idLBL";
+            idLBL.Size = new Size(56, 46);
+            idLBL.TabIndex = 19;
+            idLBL.Text = "ID";
+            // 
+            // CourseTopicForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1902, 1033);
+            Controls.Add(nameTXT);
+            Controls.Add(idTXT);
+            Controls.Add(nameLBL);
+            Controls.Add(idLBL);
+            Controls.Add(showBTN);
+            Controls.Add(courseLIST);
+            Controls.Add(courseLBL);
+            Controls.Add(returnBTN);
+            Controls.Add(topicDATA);
+            Name = "CourseTopicForm";
+            Text = "CourseTopicForm";
+            FormClosed += CourseTopicForm_FormClosed;
+            ((System.ComponentModel.ISupportInitialize)topicDATA).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button returnBTN;
+        private DataGridView topicDATA;
+        private Label courseLBL;
+        private ComboBox courseLIST;
+        private Button showBTN;
+        private RichTextBox nameTXT;
+        private RichTextBox idTXT;
+        private Label nameLBL;
+        private Label idLBL;
+    }
+}
---
+++ Desktop/Admin/CourseTopicForm/CourseTopicForm.cs
@@ -0,0 +1,76 @@
+﻿using Desktop.Data;
+using Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop.Admin.CourseTopicForm
+{
+    public partial class CourseTopicForm : Form
+    {
+        Form super;
+        ExamContext context = new();
+        public CourseTopicForm(Form super)
+        {
+            InitializeComponent();
+            this.super = super;
+            Utilites.InitForm(this, "Course and Topics Reports");
+            courseLIST.DisplayMember = "Name";
+            courseLIST.ValueMember = "ID";
+            courseLIST.DataSource = context.Courses.ToList();
+            idTXT.BackColor = nameTXT.BackColor = UIConfig.SecondaryColorDark;
+        }
+
+        private void CourseTopicForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            super.Show();
+        }
+
+        private void FillTable()
+        {
+            var topics = context.Topics.Where(top => top.CrsID == (int)courseLIST.SelectedValue);
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Name");
+            foreach (var topic in topics)
+            {
+                dt.Rows.Add(topic.ID, topic.Name);
+            }
+            topicDATA.DataSource = dt;
+            topicDATA.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            topicDATA.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            topicDATA.AllowUserToAddRows = false;
+        }
+
+        private void showBTN_Click(object sender, EventArgs e)
+        {
+            FillTable();
+        }
+
+        private void topicDATA_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == -1 && e.RowIndex != -1)
+            {
+                var dataGrid = sender as DataGridView;
+                idTXT.Text = $"{dataGrid.Rows[e.RowIndex].Cells[0].Value}";
+                nameTXT.Text = $"{dataGrid.Rows[e.RowIndex].Cells[1].Value}";
+            }
+        }
+
+        private void idTXT_MouseDown(object sender, MouseEventArgs e)
+        {
+            idLBL.Focus();
+        }
+
+        private void returnBTN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
