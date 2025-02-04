212ef324616d0664f4f82700748afc5480f3008b
Omar <tarekes68@gmail.com>
2025-02-04 04:06:28 +0200

    updated the InitForm to add icon for each form and remove the border

--- Desktop/Utilites.cs
+++ Desktop/Utilites.cs
@@ -15,10 +15,17 @@ namespace Desktop
         public static void InitForm(Form form , string name)
         {
             form.Text = name;
-            form.BackColor = UIConfig.SecondaryColorDark;
             form.StartPosition = FormStartPosition.CenterScreen;
-            form.FormBorderStyle = FormBorderStyle.FixedDialog;
             form.MaximizeBox = false;
+
+            string imagePath = Path.Combine(
+                System.Windows.Forms.Application.StartupPath.Split(@"\bin")[0],
+                "Images",
+                "iti-logo.ico"
+                );
+            form.Icon = new Icon(imagePath);
+            //form.BackColor = UIConfig.SecondaryColorDark;
+            form.FormBorderStyle = FormBorderStyle.None;
             form.WindowState = FormWindowState.Maximized;
         }
     }
