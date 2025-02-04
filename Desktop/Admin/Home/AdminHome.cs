ff0f69bea428c8c33b87b85c155a0a03feec9c86
Omar <tarekes68@gmail.com>
2025-02-04 04:05:52 +0200

    redesigned Admin Home Form completely

--- Desktop/Admin/Home/AdminHome.cs
+++ Desktop/Admin/Home/AdminHome.cs
@@ -18,12 +18,109 @@ namespace Desktop
     public partial class AdminHome : Form
     {
         Form Super;
+        ImageList imageList = new ImageList();
         public AdminHome(Form Super)
         {
             InitializeComponent();
             this.Super = Super;
             Utilites.InitForm(this, $"Welcome Admin {Utilites.Person.Name}");
+            titleLBL.Text = $"Welcome {Utilites.Person.Name}";
+
+            var buttons = new[] { crsTopBTN, examQuReport, stdAnsBTN,
+                        insCrsBTN, stdGradeBTN, stdDeptBTN, logOutBTN };
+
+            foreach (var btn in buttons)
+            {
+                btn.Tag = btn.BackColor;
+
+                btn.MouseEnter += (s, e) =>
+                {
+                    var originalColor = (Color)btn.Tag;
+                    btn.BackColor = ControlPaint.Dark(originalColor, 0.1f);
+                };
+
+                btn.MouseLeave += (s, e) =>
+                {
+                    btn.BackColor = (Color)btn.Tag;
+                };
+            }
+
+            imageList.ImageSize = new Size(128, 128);
+            string imagePath = Path.Combine(
+                Application.StartupPath.Split(@"\bin")[0],
+                "Images",
+                "book.png"
+                );
+
+            imageList.Images.Add("topic", Image.FromFile(imagePath));
+
+            imagePath = Path.Combine(
+                Application.StartupPath.Split(@"\bin")[0],
+                "Images",
+                "exam.png"
+                );
+
+            imageList.Images.Add("exam", Image.FromFile(imagePath));
+
+            imagePath = Path.Combine(
+                Application.StartupPath.Split(@"\bin")[0],
+                "Images",
+                "examAnswer.png"
+                );
+
+            imageList.Images.Add("examAnswer", Image.FromFile(imagePath));
+
+            imagePath = Path.Combine(
+                Application.StartupPath.Split(@"\bin")[0],
+                "Images",
+                "students.png"
+                );
+
+            imageList.Images.Add("students", Image.FromFile(imagePath));
+
+            imagePath = Path.Combine(
+                Application.StartupPath.Split(@"\bin")[0],
+                "Images",
+                "answering.png"
+                );
+
+            imageList.Images.Add("answering", Image.FromFile(imagePath));
+
+            imagePath = Path.Combine(
+                Application.StartupPath.Split(@"\bin")[0],
+                "Images",
+                "teaching.png"
+                );
+
+            imageList.Images.Add("teaching", Image.FromFile(imagePath));
+
+
+            crsTopBTN.ImageList = imageList;
+            crsTopBTN.ImageKey = "topic";
+            crsTopBTN.TextImageRelation = TextImageRelation.TextAboveImage;
+
+            examQuReport.ImageList = imageList;
+            examQuReport.ImageKey = "exam";
+            examQuReport.TextImageRelation = TextImageRelation.TextAboveImage;
+
+            stdAnsBTN.ImageList = imageList;
+            stdAnsBTN.ImageKey = "answering";
+            stdAnsBTN.TextImageRelation = TextImageRelation.TextAboveImage;
+
+            stdDeptBTN.ImageList = imageList;
+            stdDeptBTN.ImageKey = "students";
+            stdDeptBTN.TextImageRelation = TextImageRelation.TextAboveImage;
+
+            stdGradeBTN.ImageList = imageList;
+            stdGradeBTN.ImageKey = "examAnswer";
+            stdGradeBTN.TextImageRelation = TextImageRelation.TextAboveImage;
+
+            insCrsBTN.ImageList = imageList;
+            insCrsBTN.ImageKey = "teaching";
+            insCrsBTN.TextImageRelation = TextImageRelation.TextAboveImage;
+
         }
+
 
         private void AdminHome_FormClosed(object sender, FormClosedEventArgs e)
         {
